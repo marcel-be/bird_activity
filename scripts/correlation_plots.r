a3bb586995cf1f82e6e5f3d50d57ef866607b560
marcel-be <marcel.be@gmx.net>
2022-11-10 15:29:48 +0100

    Update

--- scripts/correlation_plots.r
+++ scripts/correlation_plots.r
@@ -1,6 +1,12 @@
-library(tidyverse)
+library(tidyr)
 library(lubridate)
+library(dplyr)
 library(data.table)
+library(hms)
+
+path<- "J:/rts/rts_activity/"
+path<- "G:/rts_activity/"
+path<- "F:/Uni_Arbeit/rts_activity/"
 
 
 df_1min<- fread(paste0(path, "bird_data_storage/tags_1_min_for_analysis.csv"))
@@ -19,8 +25,8 @@ df_1min$rdate          <- date(df_1min$date_CET)-date(ymd_hms(df_1min$start_date
 ## create 10-min-intervals for faster plotting:
 df_10min <- df_1min %>% 
   mutate(species_en = as.factor(species_en),
-         interval   = as_hms(floor_date(timestamp_CET, unit="10minutes"))) %>% 
-  group_by(ID, ring_ID, rdate, species_en,year_f,brood_patch,sex, month,week,ydate, date_f,hour,interval) %>% 
+         interval   = as_hms(floor_date(time_to_rise, unit="10minutes"))) %>% 
+  group_by(ID, ring_ID, rdate, species_en,year_f,brood_patch,sex, month,week,ydate, date_f,hour,age, sex, weight, interval) %>% 
   summarise(n_intervals=length(activity),
             n_active=length(activity[activity==1]),
             n_passive=length(activity[activity==0]),
@@ -31,31 +37,63 @@ df_10min <- df_1min %>%
 
 
 
-df_10min.r <- df_10min %>%  # Only bouts > 5 min
-  group_by(species_en, interval) %>% # year, year_f, 
+df_10min.r <- df_10min %>%  
+  group_by(ring_ID, interval) %>% # year, year_f, 
   summarise(mean_activity=mean(active_prop, na.rm = T)) %>% 
-  pivot_wider(names_from = species_en, values_from = mean_activity) %>% 
-  data.frame()
-
-plot(df_10min.r$Common_Blackbird, df_10min.r$Eurasian_Blackcap)
-plot(df_10min.r$Common_Blackbird, df_10min.r$Common_Chaffinch)
+  pivot_wider(names_from = ring_ID, values_from = mean_activity) %>% 
+  data.frame() %>% 
+  na.omit()
 
 
 
+x<- cor(df_10min.r[,2:67], use = "pairwise.complete.obs")
+na.omit()corrplot::corrplot(x,method = "ellipse", type = "lower")
 
-x<- cor(df_10min.r[,2:9], use = "pairwise.complete.obs")
-corrplot::corrplot(x,method = "ellipse", type = "lower")
 
-
-x<- corrr::correlate(df_10min.r[,3:10])#corrr::shave() %>% 
+x<- corrr::correlate(df_10min.r[,2:67])#corrr::shave() %>% 
 corrr::network_plot(x, min_cor = .01, repel = T)
 
 
+###########################
+
+meta<- read.csv(paste0(path,"bird_data_storage/tags_overview.csv"), stringsAsFactors = T)
+names<- levels(df_1min$ring_ID)
+meta<- meta %>% 
+  filter(ring_ID %in% names) %>% 
+  select(-(1:7)) %>% 
+  #select( -2, -4, -13,-14) %>% 
+  select(1,10,11,12)
+meta<- meta[which(duplicated(meta$ring_ID)==FALSE),]
+
+df_1min.r <- df_1min %>%  
+  group_by(ring_ID, time_to_rise_std) %>% # year, year_f, 
+  summarise(mean_activity=mean(activity, na.rm = T)) %>% 
+  pivot_wider(names_from = ring_ID, values_from = mean_activity) %>% 
+  data.frame()
+
+
+library(vegan)
+df_10min.r <- df_10min.r %>%
+  select(-1)
+
+nmds <- metaMDS(df_10min.r, distance = "bray", k = 2, try = 20)
+plot(nmds, display="species") # Eure Plots werden dargestellt
+text(nmds, display="species",cex=0.6, pos=4, offset=0.3)
+
+fit<- envfit(nmds, meta, permutations = 1000, na.rm = T) 
+fit
 
 
 
+decorana(veg = df_10min.r)
+pca<- rda(df_10min.r)
+ordiplot(pca, display = 'species', type = 'n')
+points(pca,display = 'species')
+text(pca, display="species",cex=0.6, pos=4, offset=0.3)
 
 
+
+## Raphael:
 # Hourly activity bouts ----
 df_bouts <- df_1min %>% #select()
   group_by(ID, rdate, species_en, ydate, hour) %>% # year, year_f,
@@ -67,8 +105,6 @@ df_bouts <- df_1min %>% #select()
             sd_bout=sd(activity_bout, na.rm = T),
             CV_bout=sd(activity_bout, na.rm = T)/mean(activity_bout, na.rm = T),
             bout_nb=n()) %>% data.frame()
-df_bouts$ydate_c <- scale(df_bouts$ydate, scale = F) # center linear date of the year
-#df_bouts$hour_sc <- df_bouts$hour-12 # Noon as intercept
 
 
 df_bouts.r <- df_bouts %>%  # Only bouts > 5 min
@@ -77,7 +113,7 @@ df_bouts.r <- df_bouts %>%  # Only bouts > 5 min
   pivot_wider(names_from = species_en, values_from = mean_bout) %>% 
   data.frame()
 
-df_bouts.r[,2:9] %>% log() %>%  cor(use = "pairwise.complete.obs") %>% 
+df_bouts.r[,3:10] %>% log() %>%  cor(use = "pairwise.complete.obs") %>% 
   corrplot::corrplot(method = "ellipse", type = "lower")
 
 x<- corrr::correlate(df_bouts.r[,3:10])#corrr::shave() %>% 
